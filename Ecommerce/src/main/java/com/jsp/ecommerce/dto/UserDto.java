a2311636873993d1e85b9deba29ee6697296f932
Rahul <rahul819723@gmail.com>
2026-01-12 22:27:21 +0530

    Admin View Merchants

---
+++ Ecommerce/src/main/java/com/jsp/ecommerce/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.jsp.ecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+	private String name;
+	private String email;
+	private Long mobile;
+	private String role;
+}
